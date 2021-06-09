5a8159132201162d7e4af7cbcb0ca73d9016b695
Vadim Prishlyak <pvadimr@pvadimr.ru>
2021-06-09 17:25:10 +0300

    Add new lines

--- info.sh
+++ info.sh
@@ -3,25 +3,38 @@
 # Purpose: Display various information
 # Author: Vadim Prishlyak <www.cyberciti.biz> under GPL v2.x+
 # -------------------------------------------------------
+echo "-------------------------------------------"
 cpu=$(</sys/class/thermal/thermal_zone0/temp)
 echo "$(date) @ $(hostname)"
 echo "Uptime: "
 uptime
 echo "OS info:"
 uname -a
+echo " "
+echo " "
 echo "-------------------------------------------"
 echo "GET TEMERATURE DATA"
 echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp)"
 echo "CPU => $((cpu/1000))'C"
+echo " "
+echo " "
 echo "-------------------------------------------"
 echo "GET STORAGE DATA"
 df -h
+echo " "
+echo " "
 echo "-------------------------------------------"
 echo "GET DOCKER DATA"
 echo "Running containers:"
 docker ps
+echo " "
+echo " "
 echo "Images:"
 docker image list
+echo " "
+echo " "
 echo "Volumes:"
 docker volume list
+echo " "
+echo " "
 echo "-------------------------------------------"
