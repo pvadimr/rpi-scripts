2f3c97942d3a5cd8ab32e68cfcd7916153a1f709
Vadim Prishlyak <pvadimr@pvadimr.ru>
2021-06-09 17:06:28 +0300

    Initial commit. Add info.sh script

---
+++ info.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Script: temprature.sh
+# Purpose: Display various information
+# Author: Vadim Prishlyak <www.cyberciti.biz> under GPL v2.x+
+# -------------------------------------------------------
+cpu=$(</sys/class/thermal/thermal_zone0/temp)
+echo "$(date) @ $(hostname)"
+echo "Uptime: "
+uptime
+echo "OS info:"
+uname -a
+echo "-------------------------------------------"
+echo "GET TEMERATURE DATA"
+echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp)"
+echo "CPU => $((cpu/1000))'C"
+echo "-------------------------------------------"
+echo "GET STORAGE DATA"
+df -h
+echo "-------------------------------------------"
+echo "GET DOCKER DATA"
+echo "Running containers:"
+docker ps
+echo "Images:"
+docker image list
+echo "Volumes:"
+docker volume list
+echo "-------------------------------------------"
